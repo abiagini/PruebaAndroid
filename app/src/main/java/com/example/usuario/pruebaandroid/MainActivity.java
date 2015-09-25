26e5b569b6ab55447c77656dd8064f121697077a
Agustin <biaginiagustin@gmail.com>
2015-09-25 14:35:54 -0300

    Tercer Commit

--- app/src/main/java/com/example/usuario/pruebaandroid/MainActivity.java
+++ app/src/main/java/com/example/usuario/pruebaandroid/MainActivity.java
@@ -66,6 +66,9 @@ public class MainActivity extends AppCompatActivity
             case 3:
                 mTitle = getString(R.string.title_section3);
                 break;
+            case 4:
+                mTitle = getString(R.string.title_section3);
+                break;
         }
     }
 
